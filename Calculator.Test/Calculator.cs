66ccf084cc1f408ba39ef313fd6bf757ffd50572
Mikko Pakkanen <mikkopak@gmail.com>
2016-12-05 13:53:51 +0200

    Add project files.

---
+++ Calculator.Test/Calculator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Calculator
+    {
+        // Metodi lukujen yhteenlaskuun
+        public int Add(int num1, int num2)
+        {
+            int result = num1 + num2;
+            return result;
+        }
+
+        /* 
+         * Metodi lukujen kertolaskuun, huom. tarkoituksella väärin 
+         * demonstraation vuoksi
+         */
+        public int Mul(int num1, int num2)
+        {
+            int result = num1 + num2;
+            return result;
+        }
+    }
+}
---
+++ Calculator.Test/CalculatorTest.cs
@@ -0,0 +1,50 @@
+﻿using NUnit.Framework;
+
+namespace Calculator.Test
+{
+    [TestFixture]
+    public class CalculatorTest
+    {
+        Calculator sut;
+
+        /* Luodaan olio testattavasta luokasta
+         * Käytettävissä kaikissa testeissä */
+        [OneTimeSetUp]
+        public void TestSetup()
+        {
+            sut = new Calculator();
+        }
+
+        /* Testaa yhteenlaskun tuloksen */
+        [Test]
+        public void ShouldAddTwoNumbers()
+        {
+            int expectedResult = sut.Add(7, 8);
+            Assert.That(expectedResult, Is.EqualTo(15));
+        }
+
+        /* Testaa kertolaskun tuloksen */
+        [Test]
+        public void ShouldMulTwoNumbers()
+        {
+            int expectedResult = sut.Mul(7, 8);
+            Assert.That(expectedResult, Is.EqualTo(56));
+        }
+
+        // Esimerkki kuinka yksittäisen testin voi jättää tekemättä
+        [Test]
+        [Ignore("Syy miksi ignorattiin")]
+        public void ShouldNotMulTwoNumbers()
+        {
+            int expectedResult = sut.Mul(7, 8);
+            Assert.That(expectedResult, Is.EqualTo(15));
+        }
+
+        // Asetaan testattavan luokan oliolle arvo null testien päätteeksi
+        [OneTimeTearDown]
+        public void TestTearDown()
+        {
+            sut = null;
+        }
+    }
+}
